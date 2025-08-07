522fbc1a65748cec08054c8fb3ae85d7131a64c3
Andre Faria <andre.faria.24@gmail.com>
2025-08-07 11:37:17 -0400

    first commit

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~>1.12.0"
+
+  cloud {
+    organization = "acfaria-hashicorp"
+
+    workspaces {
+      name = "hcp-waypoint-rds-template"
+    }
+  }
+
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "0.109.0"
+    }
+
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.68.2"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "HCP_CLIENT_ID" {
+    type = string
+    sensitive = true
+}
+
+variable "HCP_CLIENT_SECRET" {
+    type = string
+    sensitive = true
+}
+
+variable "TFE_TOKEN" {
+    type = string
+    sensitive = true
+}
